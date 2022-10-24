1dac89fad360d04911625b8aacad78e556cd3a5e
svoncoelln <89931312+svoncoelln@users.noreply.github.com>
2022-10-24 13:01:28 -0400

    Add files via upload

---
+++ webtest.ino
@@ -0,0 +1,46 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+// Set WiFi credentials
+#define WIFI_SSID "Virus.exe"
+#define WIFI_PASS "goodpassword"
+
+// Create a new web server
+ESP8266WebServer webserver(80);
+
+// Handle Root
+void rootPage() { 
+  webserver.send(200, "text/plain", "Check out https://siytek.com !"); 
+}
+
+// Handle 404
+void notfoundPage(){ 
+  webserver.send(404, "text/plain", "404: Not found"); 
+}
+
+void setup() {
+  // Setup serial port
+  Serial.begin(115200);
+  Serial.println("setup");
+
+  //Begin WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) { 
+    Serial.println("connecting...");
+    delay(100); 
+  }
+
+  // WiFi Connected
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start Web Server
+  webserver.on("/", rootPage);
+  webserver.onNotFound(notfoundPage);
+  webserver.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
