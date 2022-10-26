9ac84025593e3dcf6e003ed94d3af97f43ce4ffb
von Coelln <svoncoelln@friendsbalt.org>
2022-10-26 10:01:35 -0400

    write a message in the imperative mood

---
+++ wemos.ino/wemos.ino.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+// Set WiFi credentials
+#define WIFI_SSID "Virus.exe"
+#define WIFI_PASS "goodpassword"
+
+// Create a new web server
+ESP8266WebServer webserver(80);
+
+// Handle Root
+void rootPage() { 
+  webserver.send(200, "text/plain", "Hello World!"); 
+}
+
+// Handle 404
+void notfoundPage(){ 
+  webserver.send(404, "text/plain", "404: Not found"); 
+}
+
+void setup()
+{
+  // Setup serial port
+  Serial.begin(115200);
+  Serial.println();
+
+  //Begin WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("connecting...");
+    delay(100); 
+  }
+
+  // WiFi Connected
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start Web Server
+  webserver.on("/", rootPage);
+  webserver.onNotFound(notfoundPage);
+  webserver.begin();
+
+}
+
+// Listen for HTTP requests
+void loop(void){ 
+  webserver.handleClient(); 
+}
